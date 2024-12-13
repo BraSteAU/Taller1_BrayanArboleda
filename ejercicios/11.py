4a95be87dea9e6cd909af12d4b63dd76a4087899
BraSteAU <brayan_8924@hotmail.com>
2024-12-12 22:45:41 -0500

    Ejercicio 11 resuleto

--- ejercicios/11.py
+++ ejercicios/11.py
@@ -2,4 +2,5 @@ numero=str(int(input("Ingresa un numero")))
 nuevo=""
 for i in numero:
     nuevo=i+nuevo
-print(nuevo)
+print(f"Numero incial {numero}")
+print(f"Numero invertido {nuevo}")
---
+++ ejercicios/12.py
@@ -0,0 +1,6 @@
+lista=[4,2,6,8,1]
+mayor=lista[0]
+for i in lista:
+    if i>mayor:
+        mayor=i
+print(f"El elemento mayor de la lista {lista} es: {mayor}")
